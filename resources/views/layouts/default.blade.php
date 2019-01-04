473b1141c1d90d2d453f42036a14945141a3881f
java770520 <java770520@163.com>
2019-01-04 14:38:46 +0800

    Finish static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html lang="en">
+  <head>
+    <meta charset="utf-8"/>
+    <title>@yield('title', 'Sample') - Laravel Basic </title>
+  </head>
+  <body>
+    @yield('content')
+  </body>
+</html>
---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title', '关于')
+
+@section('content')
+<h1>About page</h1>
+@stop
