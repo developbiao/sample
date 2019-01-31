abd828c26bbc14fbe740f591d069f8b1ad32a4d1
gongbiao <developbiao@gmail.com>
2019-01-31 08:22:23 +0800

    Added comment for user policy

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -9,11 +9,13 @@ class UserPolicy
 {
     use HandlesAuthorization;
 
+    // Only current user is self can update
     public function update(User $currentUser, User $user)
     {
       return $currentUser->id === $user->id;
     }
 
+    // Only current user can delete user and can'not delete self
     public function destroy(User $currentUser, User $user)
     {
       return $currentUser->is_admin && $currentUser->id !== $user->id;
