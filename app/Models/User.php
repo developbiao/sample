d52144b3f2529ab8676f54c203f07d4f6c1ed549
gongbiao <developbiao@gmail.com>
2019-02-28 23:15:36 +0800

    Prepare user weibo statusers

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,10 @@ class User extends Authenticatable
        $this->notify(new ResetPassword($token));
     }
 
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+
+    }
+
 }
