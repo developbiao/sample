6bbafa80ee203b7e93e8b3dda2d1439beec529cf
gongbiao <developbiao@gmail.com>
2019-03-17 21:49:58 +0800

    Added publish status page

---
+++ app/Http/Controllers/StatusesController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class StatusesController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+           'content' => 'required|max:140'
+        ]);
+
+        Auth::user()->statuses()->create([
+            'content' => $request->content
+        ]);
+
+        return redirect()->back();
+    }
+}
